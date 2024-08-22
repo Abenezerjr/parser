414a3ce9f33f2dedafa746407edcbb7116dd19c5
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-08-22 22:11:34 +0300

    pdf to text phase 1

--- document/views.py
+++ document/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render , HttpResponse
-
+from pdfminer.high_level import extract_text
+from django.conf import settings
+import os
 # Create your views here.
 
 
+
+
+
 def home(request):
-  return render(request,'document/index.html')
+  file = os.path.join(settings.BASE_DIR, 'static', 'document', 'resume-sample.pdf')
+  text = extract_text(file)
+
+  context={
+    'text':text
+  }
+  return render(request,'document/index.html',context)

c62818b51295408c826e773967e36a34139957bd
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-08-23 22:58:54 +0300

    add docx to text

--- document/views.py
+++ document/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render , HttpResponse
 from pdfminer.high_level import extract_text
 from django.conf import settings
 import os
+from docx import Document
 # Create your views here.
+
+
+def extract_text_from_docx_file(docx_path):
+    doc = Document(docx_path)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
+
 
 
 
@@ -11,8 +21,16 @@ import os
 def home(request):
   file = os.path.join(settings.BASE_DIR, 'static', 'document', 'resume-sample.pdf')
   text = extract_text(file)
-
+  doc= os.path.join(settings.BASE_DIR,'static','document','resume2024.docx')
+  doc_text = extract_text_from_docx_file(doc)
   context={
-    'text':text
+    'text':text,
+    'doc_text':doc_text
   }
+
+
   return render(request,'document/index.html',context)
+
+
+
+
